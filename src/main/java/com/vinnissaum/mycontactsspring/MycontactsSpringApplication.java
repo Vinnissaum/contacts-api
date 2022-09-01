5a742f5575af136a423436eea8e9b8ad6f06d3fd
Vinicius Braz <vinicius_lsb@live.com>
2022-09-01 20:43:28 -0300

    init project

---
+++ src/main/java/com/vinnissaum/mycontactsspring/MycontactsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.vinnissaum.mycontactsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycontactsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycontactsSpringApplication.class, args);
+	}
+
+}
