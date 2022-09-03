24ba278d05501236b9cdd6080abf8bda5bde3fe5
Vinicius Braz <vinicius_lsb@live.com>
2022-09-03 16:38:31 -0300

    feat: add Category store on controller layer

--- src/main/java/com/vinnissaum/mycontactsspring/controllers/CategoryController.java
+++ src/main/java/com/vinnissaum/mycontactsspring/controllers/CategoryController.java
@@ -1,13 +1,17 @@
 package com.vinnissaum.mycontactsspring.controllers;
 
+import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vinnissaum.mycontactsspring.entities.Category;
 import com.vinnissaum.mycontactsspring.services.CategoryService;
@@ -24,12 +28,21 @@ public class CategoryController {
     @GetMapping
     public ResponseEntity<List<Category>> index() {
         List<Category> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        return ResponseEntity.ok(list);
     }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Category> show(@PathVariable UUID id) {
         Category category = service.findById(id);
-        return ResponseEntity.ok().body(category);
+        return ResponseEntity.ok(category);
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> store(@RequestBody Category cat) {
+        Category category = service.create(cat);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+        .buildAndExpand(cat.getId()).toUri();
+
+        return ResponseEntity.created(location).body(category);
     }
 }
