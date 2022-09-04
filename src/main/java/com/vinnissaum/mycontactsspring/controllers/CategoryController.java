cdf527942b751ccbe4c86485d95e4b0085d20afe
Vinicius Braz <vinicius_lsb@live.com>
2022-09-03 22:29:19 -0300

    feat: add update and delete Category on controller

--- src/main/java/com/vinnissaum/mycontactsspring/controllers/CategoryController.java
+++ src/main/java/com/vinnissaum/mycontactsspring/controllers/CategoryController.java
@@ -4,10 +4,13 @@ import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,5 +47,19 @@ public class CategoryController {
         .buildAndExpand(cat.getId()).toUri();
 
         return ResponseEntity.created(location).body(category);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Category> update(@PathVariable UUID id,
+    @RequestBody Category cat)
+    {
+        Category category = service.update(id, cat);
+        return ResponseEntity.ok(category);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<HttpStatus> delete(@PathVariable UUID id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
