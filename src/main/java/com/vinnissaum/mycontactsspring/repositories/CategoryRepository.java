a48da611a2a060a8f6082e4dcb27f23024913d99
Vinicius Braz <vinicius_lsb@live.com>
2022-09-07 22:29:13 -0300

    refactor: add Repository annotation

--- src/main/java/com/vinnissaum/mycontactsspring/repositories/CategoryRepository.java
+++ src/main/java/com/vinnissaum/mycontactsspring/repositories/CategoryRepository.java
@@ -3,7 +3,9 @@ package com.vinnissaum.mycontactsspring.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.vinnissaum.mycontactsspring.entities.Category;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID>{}
--- src/main/java/com/vinnissaum/mycontactsspring/repositories/ContactRepository.java
+++ src/main/java/com/vinnissaum/mycontactsspring/repositories/ContactRepository.java
@@ -3,7 +3,9 @@ package com.vinnissaum.mycontactsspring.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.vinnissaum.mycontactsspring.entities.Contact;
 
+@Repository
 public interface ContactRepository extends JpaRepository<Contact, UUID>{}
